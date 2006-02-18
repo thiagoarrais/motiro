e873f430298cd25df15fe703c2830fc16634fe67
thiagoarrais <thiagoarrais@31482026-630c-0410-91bb-adf60aae4351>
2006-02-18 13:59:20 +0000

    Reporter SVN lendo o nome do autor da modificacao
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@12 31482026-630c-0410-91bb-adf60aae4351

--- src/app/reporters/svn_reporter.rb
+++ src/app/reporters/svn_reporter.rb
@@ -1,12 +1,9 @@
 class Headline
 
-    def author
-    end
+    attr_reader :author, :date, :title
 
-    def date
-    end
-
-    def title
+    def initialize(author)
+        @author = author
     end
 
 end
@@ -14,10 +11,20 @@ end
 class SubversionReporter
 
     def initialize(connection)
+        @connection = connection
     end
 
     def latest_headline
-        Headline.new
+        return svn_parse_log(@connection.log, 1)
+    end
+    
+private
+    
+    def svn_parse_log(text, num)
+        remain = /^-+\n/.match(text).post_match
+        md = /^r\d+\s*\|\s*(\w+)\s*\|/.match(remain)
+        author = md[1]
+        Headline.new(author)
     end
 
 end
--- src/test/stubs/svn_connection.rb
+++ src/test/stubs/svn_connection.rb
@@ -1,11 +1,13 @@
 class StubSVNConnection
 
+    attr_reader :log
+
     def initialize
         @log = ''
     end
 
     def log_append_line(text)
-        @log += text + '\n'
+        @log += text + "\n"
     end
-
+    
 end
