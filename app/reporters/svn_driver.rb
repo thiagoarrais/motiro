d27dd0e8cdd14aa22037944f49349ba8304305a2
thiagoarrais <thiagoarrais@31482026-630c-0410-91bb-adf60aae4351>
2006-03-08 17:43:08 +0000

    Versão inicial com cache
    Os testes estao verdes novamente
    
    
    
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@44 31482026-630c-0410-91bb-adf60aae4351

--- app/reporters/svn_driver.rb
+++ app/reporters/svn_driver.rb
@@ -1,5 +1,7 @@
 require 'reporters/svn_reporter'
 
+require 'headline'
+
 class SubversionDriver
 
     def initialize(reporter = SubversionReporter.new)
@@ -11,7 +13,13 @@ class SubversionDriver
         hls = @reporter.latest_headlines 5
         
         hls.each do |hl|
-            hl.save
+            #TODO move this to the headline class itself
+            rs = Headline.find(:all,
+                      :conditions => "    author = '#{hl.author}' " +
+                                     "and event_date = '#{hl.event_date}' " +
+                                     "and title = '#{hl.title}'")
+                                     
+            hl.save if rs.empty?
         end
     end
 
--- test/mocks/headline.rb
+++ test/mocks/headline.rb
@@ -6,9 +6,16 @@ class MockHeadline < Headline
 
     include Test::Unit::Assertions
 
+    @@obj_count = 0
+
     def initialize
+        super(:author => 'unknown',
+              :event_date => DateTime.new(2006 + @@obj_count, 3, 8),
+              :title => 'untitled')
         @expected_times_save_called = 0
         @actual_times_save_called = 0
+        
+        @@obj_count += 1
     end
 
     def expect_save
@@ -16,6 +23,7 @@ class MockHeadline < Headline
     end
     
     def save
+        super
         @actual_times_save_called += 1
     end
     
