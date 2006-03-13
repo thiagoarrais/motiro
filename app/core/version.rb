da11701e77bcd38b913a3ecb3de57140b7e447f7
thiagoarrais <thiagoarrais@31482026-630c-0410-91bb-adf60aae4351>
2006-03-13 23:25:50 +0000

    Numero de revisao removido
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@66 31482026-630c-0410-91bb-adf60aae4351

--- app/core/version.rb
+++ app/core/version.rb
@@ -1,10 +1,1 @@
-def get_revision_from_subversion
-    basedir = File.expand_path(__FILE__ + '/..')
-    svn_result = `svn info --xml #{basedir}`
-    md = /<commit\s+revision=\"(\d+)\">/.match(svn_result)
-    return md[1]
-end
-
-
 MOTIRO_VERSION = '0.1'
-MOTIRO_REVISION = get_revision_from_subversion
