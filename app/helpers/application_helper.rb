d5c345ecd512b6f95761ecdca8b347168a6ae39c
thiagoarrais <thiagoarrais@31482026-630c-0410-91bb-adf60aae4351>
2006-03-15 16:57:55 +0000

    Template de RSS traduzido para builder e URL do servidor gerada dinamicamente
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@70 31482026-630c-0410-91bb-adf60aae4351

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+  def server_url_for(options = {})
+    url_for options.update(:only_path => false)
+  end
+
+
 end
