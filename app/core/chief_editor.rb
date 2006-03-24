0e0a9d9a739a24d683c996cb1919545280b55d64
thiagoarrais <thiagoarrais@31482026-630c-0410-91bb-adf60aae4351>
2006-03-24 14:22:06 +0000

    Introduzi um padrao strategy no editor-chefe
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@101 31482026-630c-0410-91bb-adf60aae4351

--- app/core/chief_editor.rb
+++ app/core/chief_editor.rb
@@ -8,7 +8,8 @@ class ChiefEditor
     def initialize(settings=SubversionSettingsProvider.new)
         @settings = settings
         @reporters = Hash.new
-
+        @strategy = create_strategy
+                
         #TODO Maybe we'll need something like a reporter fetcher that updates
         #     the reporter list at server startup time
         #     Or maybe the reporters can be 'require'd dinamically as needed
@@ -16,36 +17,65 @@ class ChiefEditor
     end
 
     def latest_news_from(reporter_name)
-        if (@settings.getUpdateInterval == 0) then
-            # we're in development mode
-            # every request should be routed directly to the reporter
-            reporter = @reporters[reporter_name]
-            return reporter.latest_headlines
-        else 
-            return Headline.latest(@settings.getPackageSize)
-        end
+        @strategy.latest_news_from(reporter_name)
     end
     
     def article_for_headline(reporter_name, rid)
-        if (@settings.getUpdateInterval == 0) then
-            # we're in development mode
-            # every request should be routed directly to the reporter
-            reporter = @reporters[reporter_name]
-
-            return reporter.article_for(rid)
-        else 
-            headline=  Headline.find(:first,
-                                     :conditions => ["reported_by = ? " +
-                                                     "and rid = ?",
-                                                     reporter_name,
-                                                     rid])
-            return headline.article
-        end
+        @strategy.article_for_headline(reporter_name, rid)
     end
     
     # Adds the given reporter to the set of reporters employed by the editor
     def employ(reporter)
         @reporters.update(reporter.name => reporter)
+    end
+
+private
+    
+    def create_strategy
+        if (@settings.getUpdateInterval == 0) then
+            return LiveEditorStrategy.new(@reporters)
+        else
+            return CachedEditorStrategy.new(@settings)
+        end 
+    end
+    
+end
+
+class LiveEditorStrategy
+
+    def initialize(reporter_map)
+        @reporters = reporter_map
+    end
+    
+    def latest_news_from(reporter_name)
+        reporter = @reporters[reporter_name]
+        return reporter.latest_headlines
+    end
+    
+    def article_for_headline(reporter_name, rid)
+        reporter = @reporters[reporter_name]
+        return reporter.article_for(rid)
+    end
+
+end
+
+class CachedEditorStrategy
+    
+    def initialize(settings)
+        @settings = settings
+    end
+
+    def latest_news_from(reporter_name)
+        return Headline.latest(@settings.getPackageSize)
+    end    
+    
+    def article_for_headline(reporter_name, rid)
+        headline=  Headline.find(:first,
+                                 :conditions => ["reported_by = ? " +
+                                                 "and rid = ?",
+                                                 reporter_name,
+                                                 rid])
+        return headline.article
     end
 
 end
