332ef9b3e8efdb8f9d5e4a5f3681696da19a8bce
thiagoarrais <thiagoarrais@31482026-630c-0410-91bb-adf60aae4351>
2006-04-03 14:58:46 +0000

    Teste para reporter de eventos ler da base de dados
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@127 31482026-630c-0410-91bb-adf60aae4351

--- test/unit/events_reporter_test.rb
+++ test/unit/events_reporter_test.rb
@@ -4,10 +4,23 @@ require 'reporters/events_reporter'
 
 class EventsReporterTest < Test::Unit::TestCase
 
+    fixtures :headlines
+
     def test_channel_title
         reporter = EventsReporter.new
         
         assert_equal 'Próximos eventos', reporter.channel_title
+    end
+    
+    def test_retrieves_from_database
+        release_event = headlines('release_event')
+        meeting_event = headlines('meeting_event')
+        reporter = EventsReporter.new
+        
+        headlines = reporter.latest_headlines
+
+        assert headlines.include?(release_event)
+        assert headlines.include?(meeting_event)
     end
     
 end
