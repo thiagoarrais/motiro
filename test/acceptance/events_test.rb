92a4bca1fb48378f47c34048d2bada27a0c4f71c
thiagoarrais <thiagoarrais@31482026-630c-0410-91bb-adf60aae4351>
2006-04-19 20:21:53 +0000

    Portando os testes de aceitacao para selenium-rc
    Somente os testes em test_main_page foram portados ateh agora. Os demais
    podem falhar.
    
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@149 31482026-630c-0410-91bb-adf60aae4351

--- test/acceptance/events_test.rb
+++ test/acceptance/events_test.rb
@@ -1,12 +1,27 @@
+require File.dirname(__FILE__) + '/../test_helper'
+
+require 'test/unit'
+require '/home/thiagob/programas/selenium-remote-control-0.7.1/ruby/selenium'
+
 require 'acceptance/live_mode_test'
 
 class MotiroAcceptanceTest < Test::Unit::TestCase
 
     include LiveModeTestCase
 
+    def do_setup
+        @sel = Selenium::SeleneseInterpreter.new("localhost", 4444,
+                        "*firefox", "http://localhost:3000", 15000)
+        @sel.start
+    end
+  
     def test_no_error_on_development_mode
-        open '/report/events'
-        assertTextPresent 'Próximos eventos'
+        @sel.open '/report/events'
+        @sel.assert_text_present 'Próximos eventos'
+    end
+    
+    def do_teardown
+        @sel.stop
     end
 
 end
--- test/acceptance/main_page_test.rb
+++ test/acceptance/main_page_test.rb
@@ -1,31 +1,52 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require File.dirname(__FILE__) + "/../../vendor/selenium/selenium"
+
 class MotiroAcceptanceTest < Test::Unit::TestCase
 
+    def setup
+        @sel = Selenium::SeleneseInterpreter.new("localhost", 4444,
+                        "*firefox", "http://localhost:3000", 15000)
+        @sel.start
+    end
+  
     def test_main_page
-        open '/'
-        assertTitle "Motiro: Bem-vindo"
-        assertElementPresent "//div[@id='description']"
-        assertTextPresent "Seja bem-vindo!"
-        assertTextPresent "Motiro versão 0.2"
+        @sel.open '/'
+        assert_equal "Motiro: Bem-vindo", @sel.get_title
+        @sel.assert_element_present "//div[@id='description']"
+        @sel.assert_text_present "Seja bem-vindo!"
+        @sel.assert_text_present 'Motiro vers' #Motiro versão 0.2
+        @sel.assert_text_present '0.2'
     end
         
     def test_report_html
-        open '/report/subversion'
-        assertTextPresent 'Últimas notícias do Subversion'
-        assertElementPresent "//img[starts-with(@src, '/images/rss.gif')]"
-        clickAndWait "//img[starts-with(@src, '/images/rss.gif')]"
-        assertLocation "/feed/subversion"
+        @sel.open '/report/subversion'
+        @sel.assert_text_present 'cias do Subversion' #Últimas notícias do Subversion
+        @sel.assert_element_present "//img[starts-with(@src, '/images/rss.gif')]"
+        @sel.click "//img[starts-with(@src, '/images/rss.gif')]"
+        @sel.wait_for_page_to_load(500)
+        @sel.assert_location "/feed/subversion"
     end
     
     def test_subversion_on_main
-        open('/')
-        assertElementPresent "//div[@id = 'svn']"
-        assertText "//div[@id = 'svn']", 'regexp:Últimas notícias do Subversion'
+        @sel.open('/')
+        @sel.assert_element_present "//div[@id = 'svn']"
+        assert @sel.get_text("//div[@id = 'svn']").match(/cias do Subversion/) #regexp:Últimas notícias do Subversion
     end
     
     def test_events_on_main
-        open('/')
-        assertElementPresent "//div[@id = 'events']"
-        assertText "//div[@id = 'events']", 'regexp:Próximos eventos'
+        @sel.open('/')
+        @sel.assert_element_present "//div[@id = 'events']"
+        assert @sel.get_text("//div[@id = 'events']").match(/ximos eventos/) #regexp:Próximos eventos
+    end
+    
+    def teardown
+        @sel.stop
+    end
+
+private
+    
+    def method_missing(name, *args)
+        @sel.send(name, args)
     end
     
 end
