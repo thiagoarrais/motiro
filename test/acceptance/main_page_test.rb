bcceab498722fced5f6654e099e067609ef3c1c3
tbasouza <tbasouza@31482026-630c-0410-91bb-adf60aae4351>
2006-09-05 17:11:14 +0000

    Upgraded version number to 0.5
    Also took the time to tidy up the licensing stuff
    
    --- pt-br --------------------
    
    Numero de versao atualizado para 0.5
    
    Aproveitei para dar uma ajeitada nas coisas de licenciamento.
    
    
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@314 31482026-630c-0410-91bb-adf60aae4351

--- test/acceptance/main_page_test.rb
+++ test/acceptance/main_page_test.rb
@@ -1,10 +1,27 @@
+#  Motiro - A project tracking tool
+#  Copyright (C) 2006  Thiago Arrais
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 class MainPageAcceptanceTest < SeleniumTestCase
 
     fixtures :users
     
     def test_version_number
         open '/en'
-        assert_text_present 'Motiro version 0.4.3'
+        assert_text_present 'Motiro version 0.5'
     end
         
     def test_report_html
@@ -124,5 +141,4 @@ class MainPageAcceptanceTest < SeleniumTestCase
         Page.connection.commit_db_transaction
     end
 
-    
 end
