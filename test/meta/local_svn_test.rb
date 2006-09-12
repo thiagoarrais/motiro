30d1a8c59a5e0058d7f2d37d99e801447f29cb3b
tbasouza <tbasouza@31482026-630c-0410-91bb-adf60aae4351>
2006-09-12 16:59:55 +0000

    Identing and licensing
    --- pt-br ---------------
    
    Identacao e licenca
    
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@327 31482026-630c-0410-91bb-adf60aae4351

--- test/meta/local_svn_test.rb
+++ test/meta/local_svn_test.rb
@@ -1,107 +1,124 @@
+#  Motiro - A project tracking tool
+#  Copyright (C) 2006  Thiago Arrais
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 require File.dirname(__FILE__) + '/../test_helper'
 
 require 'local_svn'
 
 class LocalSubversionRepositoryTest < Test::Unit::TestCase
+  
+  def setup
+    @repo = LocalSubversionRepository.new
+  end
+  
+  def test_readable_to_anonymous
+    assert_equal '', execute("svn ls #{@repo.url}") # no error message
+  end
+  
+  def test_create_and_destroy
+    @repo.destroy
     
-    def setup
-        @repo = LocalSubversionRepository.new
-    end
-
-    def test_readable_to_anonymous
-        assert_equal '', execute("svn ls #{@repo.url}") # no error message
-    end
+    assert '' != execute("svn ls #{@repo.url}") # some error message
+  end
+  
+  def test_make_remote_dir
+    @repo.mkdir('testdir', 'directory for tests created')
     
-    def test_create_and_destroy
-        @repo.destroy
-        
-        assert '' != execute("svn ls #{@repo.url}") # some error message
-    end
+    assert "testdir/\n" == execute("svn ls #{@repo.url}")
+  end
+  
+  def test_make_local_dir
+    @repo.mkdir('not_yet_commited_dir')
     
-    def test_make_remote_dir
-        @repo.mkdir('testdir', 'directory for tests created')
-
-        assert "testdir/\n" == execute("svn ls #{@repo.url}")
-    end
+    assert '' == execute("svn ls #{@repo.url}")
     
-    def test_make_local_dir
-        @repo.mkdir('not_yet_commited_dir')
-        
-        assert '' == execute("svn ls #{@repo.url}")
-        
-        @repo.commit('created a new dir')
-        
-        assert_equal "not_yet_commited_dir/\n", execute("svn ls #{@repo.url}")
-    end
+    @repo.commit('created a new dir')
     
-    def test_add_file
-        @repo.add_file('local_file.txt', "the file is stored locally until\n" +
+    assert_equal "not_yet_commited_dir/\n", execute("svn ls #{@repo.url}")
+  end
+  
+  def test_add_file
+    @repo.add_file('local_file.txt', "the file is stored locally until\n" +
                                          "I choose to commit it")
-
-        assert '' == execute("svn ls #{@repo.url}")
-        
-        @repo.commit('added a file')
-        
-        assert_equal "local_file.txt\n", execute("svn ls #{@repo.url}")
-    end
     
-    def test_modify_file_with_put
-        @repo.put_file('local_file.txt', "this is line number 1\n" +
+    assert '' == execute("svn ls #{@repo.url}")
+    
+    @repo.commit('added a file')
+    
+    assert_equal "local_file.txt\n", execute("svn ls #{@repo.url}")
+  end
+  
+  def test_modify_file_with_put
+    @repo.put_file('local_file.txt', "this is line number 1\n" +
                                          "this is line number 2\n") 
-
-        assert '' == execute("svn ls #{@repo.url}")
-        
-        @repo.commit('added a new file')
-        
-        assert_equal "local_file.txt\n", execute("svn ls #{@repo.url}")
-        
-        @repo.put_file('local_file.txt', "this is line number 1\n" +
+    
+    assert '' == execute("svn ls #{@repo.url}")
+    
+    @repo.commit('added a new file')
+    
+    assert_equal "local_file.txt\n", execute("svn ls #{@repo.url}")
+    
+    @repo.put_file('local_file.txt', "this is line number 1\n" +
                                          "this is the new line number 2\n") 
-                                         
-        @repo.commit('added a new file')
-                                         
-        regexp = Regexp.new("Index: local_file.txt\n" +
-                     "===================================================================\n" +
-                     "--- local_file.txt\t\\([^1]+1\\)\n" +
-                     "\\+\\+\\+ local_file.txt\t\\([^2]+2\\)\n" +
-                     "@@ -1,2 \\+1,2 @@\n" +
-                     " this is line number 1\n" +
-                     "-this is line number 2\n" +
-                     "\\+this is the new line number 2")
-        assert_not_nil regexp.match(execute("svn diff -r1:2 #{@repo.url}"))
-    end
     
-    def test_remote_copy
-        @repo.add_file('fileA.txt', 'contents')
-        @repo.commit('added file A')
-        
-        @repo.copy('fileA.txt', 'fileB.txt', 'copied file A to B')
-        
-        assert_equal "fileA.txt\nfileB.txt\n", execute("svn ls #{@repo.url}")
-        regexp = /A \/fileB.txt \(\w+ \/fileA.txt:1\)$/
-        assert_not_nil regexp.match(execute("svn log #{@repo.url} -vr2"))
-    end
+    @repo.commit('added a new file')
     
-    def test_remote_move
-        @repo.add_file('fileA.txt', 'contents')
-        @repo.commit('added file A')
-        
-        @repo.move('fileA.txt', 'fileC.txt', 'moved file A to C')
-        
-        assert_equal "fileC.txt\n", execute("svn ls #{@repo.url}")
-        regexp = /A \/fileC.txt \(\w+ \/fileA.txt:1\)$/
-        assert_not_nil regexp.match(execute("svn log #{@repo.url} -vr2"))
-    end
-        
-    def teardown
-        @repo.destroy
-    end
+    regexp = Regexp.new("Index: local_file.txt\n" +
+                        "===================================================================\n" +
+                        "--- local_file.txt\t\\([^1]+1\\)\n" +
+                        "\\+\\+\\+ local_file.txt\t\\([^2]+2\\)\n" +
+                        "@@ -1,2 \\+1,2 @@\n" +
+                        " this is line number 1\n" +
+                        "-this is line number 2\n" +
+                        "\\+this is the new line number 2")
+    assert_not_nil regexp.match(execute("svn diff -r1:2 #{@repo.url}"))
+  end
+  
+  def test_remote_copy
+    @repo.add_file('fileA.txt', 'contents')
+    @repo.commit('added file A')
     
-private
-
-    def execute(command_line)
-        pio = IO.popen("#{command_line} 2>&1")
-        return pio.read
-    end
+    @repo.copy('fileA.txt', 'fileB.txt', 'copied file A to B')
     
+    assert_equal "fileA.txt\nfileB.txt\n", execute("svn ls #{@repo.url}")
+    regexp = /A \/fileB.txt \(\w+ \/fileA.txt:1\)$/
+    assert_not_nil regexp.match(execute("svn log #{@repo.url} -vr2"))
+  end
+  
+  def test_remote_move
+    @repo.add_file('fileA.txt', 'contents')
+    @repo.commit('added file A')
+    
+    @repo.move('fileA.txt', 'fileC.txt', 'moved file A to C')
+    
+    assert_equal "fileC.txt\n", execute("svn ls #{@repo.url}")
+    regexp = /A \/fileC.txt \(\w+ \/fileA.txt:1\)$/
+    assert_not_nil regexp.match(execute("svn log #{@repo.url} -vr2"))
+  end
+  
+  def teardown
+    @repo.destroy
+  end
+  
+  private
+  
+  def execute(command_line)
+    pio = IO.popen("#{command_line} 2>&1")
+    return pio.read
+  end
+  
 end
