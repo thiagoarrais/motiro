a0f0400c7caefb225a1377ab157798e93f871526
tbasouza <tbasouza@31482026-630c-0410-91bb-adf60aae4351>
2006-10-23 15:57:16 +0000

    Just reformatting svn_connection.rb
    --- pt-br -----------
    
    Apenas reformatando o arquivo svn_connection.rb
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@363 31482026-630c-0410-91bb-adf60aae4351

--- app/reporters/svn_connection.rb
+++ app/reporters/svn_connection.rb
@@ -19,40 +19,39 @@ require 'ports/runner'
 require 'reporters/svn_settings'
 
 class SubversionConnection
-    
-    def initialize(settings=SubversionSettingsProvider.new, runner=Runner.new)
-        @settings = settings
-        @runner = CustomEnvironmentRunner.new(runner, 'LC_MESSAGES' => 'C')
-        @diff_cache = Hash.new
-    end
-
-    def log(rev_id=nil)
-        command = "svn log #{@settings.repo_url} -v "
-        if rev_id.nil? 
-            command += "--limit=#{@settings.package_size}"
-        else
-            command += "-r#{rev_id.to_s}"
-        end
-        
-        @runner.run(command)
+  
+  def initialize(settings=SubversionSettingsProvider.new, runner=Runner.new)
+    @settings = settings
+    @runner = CustomEnvironmentRunner.new(runner, 'LC_MESSAGES' => 'C')
+    @diff_cache = Hash.new
+  end
+  
+  def log(rev_id=nil)
+    command = "svn log #{@settings.repo_url} -v "
+    if rev_id.nil? 
+      command += "--limit=#{@settings.package_size}"
+    else
+      command += "-r#{rev_id.to_s}"
     end
     
-    def diff(rev_id)
-        cached_result = @diff_cache[rev_id]
-        
-        if cached_result.nil?
-            command = "svn diff #{@settings.repo_url} -r#{rev_id.to_i - 1}:#{rev_id}"
+    @runner.run(command)
+  end
+  
+  def diff(rev_id)
+    cached_result = @diff_cache[rev_id]
     
-            cached_result = @runner.run command
-            @diff_cache[rev_id] = cached_result
-        end
-        
-        return cached_result
+    if cached_result.nil?
+      command = "svn diff #{@settings.repo_url} -r#{rev_id.to_i - 1}:#{rev_id}"
+      
+      cached_result = @runner.run command
+      @diff_cache[rev_id] = cached_result
     end
     
-    def info(path, rev_id)
-        command = "svn info -r#{rev_id} --xml #{@settings.repo_url}#{path}"
-        @runner.run(command)
-    end
-    
+    return cached_result
+  end
+  
+  def info(path, rev_id)
+    @runner.run("svn info -r#{rev_id} --xml #{@settings.repo_url}#{path}")
+  end
+  
 end                                                           
