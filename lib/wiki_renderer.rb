3eadcdcfa658cbbf018119a5bff9ef46ab8627de
tbasouza <tbasouza@31482026-630c-0410-91bb-adf60aae4351>
2007-01-31 14:23:29 +0000

    Resummons WikiRenderer from oblivion
    Sorry, forgot to add the renderer with on r481
    
    --- pt-br -----------
    
    Reinvoca o WikiRenderer do limbo
    
    Foi mal, esqueci de adicionar o renderizador na r481
    
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@483 31482026-630c-0410-91bb-adf60aae4351

---
+++ lib/wiki_renderer.rb
@@ -0,0 +1,31 @@
+#  Motiro - A project tracking tool
+#  Copyright (C) 2006-2007  Thiago Arrais
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require 'rubygems'
+require 'mediacloth'
+
+class WikiRenderer
+
+  include MediaCloth
+
+  def render_html(text, locale_code=nil)
+    translator = Translator.for(locale_code)
+    wiki_text = translator.localize(text).delete("\r")
+    wiki_to_html(wiki_text)
+  end
+
+end
---
+++ test/unit/wiki_renderer_test.rb
@@ -0,0 +1,55 @@
+#  Motiro - A project tracking tool
+#  Copyright (C) 2006-2007  Thiago Arrais
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+require File.dirname(__FILE__) + '/../test_helper'
+
+class WikiRendererTest < Test::Unit::TestCase
+
+  attr_reader :renderer
+  
+  def setup
+    @renderer = WikiRenderer.new
+  end
+
+  def test_renders_title
+    assert_equal "<h1>Motiro</h1>", renderer.render_html('= Motiro =')
+  end
+  
+  def test_breaks_paragraphs_on_linebreak_and_return_feed
+    line_break_only_text = "= Motiro =\n\nThis is project Motiro"
+    feed_return_text = "= Motiro =\r\n\r\nThis is project Motiro"
+    expected_text = '<h1>Motiro</h1><p>This is project Motiro</p>'
+
+    assert_equal expected_text, renderer.render_html(line_break_only_text)
+    assert_equal expected_text, renderer.render_html(feed_return_text)
+  end
+  
+  def test_render_external_links
+    expected = "<p><a href=\"http://nowhere.com\" rel=\"nofollow\">Nowhere</a></p>"
+    assert_equal expected, renderer.render_html('[http://nowhere.com Nowhere]')
+  end
+  
+  def test_renders_multiple_languages
+    wiki_text = "Bem-vindo ao Motiro\n\n" +
+                "--- en ---\n" +
+                "Welcome to Motiro"
+    assert_equal "<p>Bem-vindo ao Motiro</p>", renderer.render_html(wiki_text)
+    assert_equal "<p>Welcome to Motiro</p>",
+                 renderer.render_html(wiki_text, 'en')
+  end
+  
+end
