5ccb83481aa34558eada66d951c88f7527739bfd
tbasouza <tbasouza@31482026-630c-0410-91bb-adf60aae4351>
2007-02-13 14:21:59 +0000

    Fixes issue with Gentoo's Darcs 1.0.5
    --- pt-br -------------------
    
    Conserta o problema com Darcs 1.0.5 no Gentoo
    
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@513 31482026-630c-0410-91bb-adf60aae4351

--- app/reporters/darcs_temp_repo.rb
+++ app/reporters/darcs_temp_repo.rb
@@ -1,5 +1,22 @@
+#  Motiro - A project tracking tool
+#  Copyright (C) 2006-2007  Thiago Arrais
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 require 'fileutils'
-require 'ports/runner'
+require 'ports/chdir_runner'
 
 # This class is part of a workaround for darcs bug #250
 # Darcs somehow needs to have a local repo to retrieve the changelog from a
@@ -8,14 +25,16 @@ require 'ports/runner'
 # For more details see http://bugs.darcs.net/issue250
 class DarcsTempRepo
 
-  attr_reader :path
+  @@path = File.expand_path(__FILE__ + '../../../../tmp/tmprepo')
 
-  def initialize(filesystem=File, fileutils=FileUtils, runner=Runner.new)
-    @path = File.expand_path(__FILE__ + '../../../../tmp/tmprepo')
-    unless filesystem.exists?(@path + '/_darcs')
-      fileutils.mkdir_p(@path)
-      runner.run("darcs init --repodir=\"#{@path}\"")
+  def initialize(filesystem=File, fileutils=FileUtils,
+                 runner=ChdirRunner.new(@@path))
+    unless filesystem.exists?(path + '/_darcs')
+      fileutils.mkdir_p(path)
+      runner.run("darcs init")
     end
   end
+
+  def path; @@path; end
 
 end
