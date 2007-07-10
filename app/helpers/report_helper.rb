def06faa716a1246fdeb217d3a1e46361f798592
tbasouza <tbasouza@31482026-630c-0410-91bb-adf60aae4351>
2007-07-10 20:11:42 +0000

    An acceptable diff table, finally
    Many thanks to the [http://www.review-board.org/ Review Board] team, from whom
    I shamelessly stole a lot of ideas (and also some code snippets)
    
    --- pt-br ---------
    
    Finalmente uma tabela de comparacao aceitavel
    
    Um muito obrigado para a equipe do [http://www.review-board.org/ Review Board],
    de quem eu na cara de pau roubei um monte de ideias (e tambem alguns trechos
    de codigo)
    
    
    git-svn-id: https://svn.code.sf.net/p/motiro/code/trunk@626 31482026-630c-0410-91bb-adf60aae4351

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -23,23 +23,29 @@ module ReportHelper
                                :class => "diff-window") do |b|
       b.h2 'Changes to %s' / change.resource_name
       b.table :class => 'diff', :cellspacing => '0' do
+        b.colgroup do
+          b.col :class => 'line_number'
+          b.col :class => 'left'
+          b.col :class => 'right'
+          b.col :class => 'line_number'
+        end
         change.chunked_diff.each do |chunk|
           if chunk.separator?
             b.tbody :class => 'separator' do
               b.tr do
-                b.td('...', :class => 'line_number')
+                b.td
                 b.td('%d more lines' / chunk.num_lines, :colspan => '2')
-                b.td('...', :class => 'line_number')
+                b.td
               end
             end
           else  
             b.tbody :class => chunk.action.to_s do
               chunk.lines.each do |line|
                 b.tr do
-                  b.td(:class => 'line_number') {b << (line.original_position || '&nbsp;').to_s}
+                  b.td {b << (line.original_position || '&nbsp;').to_s}
                   b.td {b.pre{b << (h(line.original_text) || '&nbsp;')}}
                   b.td {b.pre{b << (h(line.modified_text) || '&nbsp;')}}
-                  b.td(:class => 'line_number') {b << (line.modified_position || '&nbsp;').to_s}
+                  b.td {b << (line.modified_position || '&nbsp;').to_s}
                 end
               end
             end
